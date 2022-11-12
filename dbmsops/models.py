50ce42aeff5c0cfacddcc8f497e8ac5be7fe9eb5
ayushbudh <ayushbudhwani41@gmail.com>
2022-11-12 16:22:31 -0500

    connected database instance, created Passenger model for testing, re-generated secret_key and added environment variables

--- dbmsops/models.py
+++ dbmsops/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Passenger(models.Model):
+    passenger_id = models.IntegerField()
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
