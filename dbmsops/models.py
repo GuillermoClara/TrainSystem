497d7f22b61b9c469130e5c6a120f2aef148f6e0
ayushbudh <ayushbudhwani41@gmail.com>
2022-11-14 21:50:32 -0500

    changed db name from dbmsops_passengers to passengers, created dynamic dropdown UI for different forms for insert operations

--- dbmsops/models.py
+++ dbmsops/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class Passenger(models.Model):
+    class Meta:
+        db_table = 'passenger'
     passenger_id = models.IntegerField()
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
