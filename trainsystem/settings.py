e039622af802b93c3e2263b791bbe183f4cb451b
ayushbudh <ayushbudhwani41@gmail.com>
2022-11-25 15:54:14 -0500

    fixed allowed_hosts

--- trainsystem/settings.py
+++ trainsystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://train-dbms.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['train-dbms.herokuapp.com', 'localhost']
 
 
 # Application definition
